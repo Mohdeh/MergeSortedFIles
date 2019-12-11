79f50fec856d12c564d22fdd01ce6f5f571bffc1
Mohsen Dehhaghi <dehhaghi@usc.edu>
2019-12-10 16:47:45 -0800

    Add files via upload
    Given two sorted files, a Java program is written to merge them to preserve sort order. Assume we can compare two lines of a file as strings.
    No assumption that either of the files can fit in the memory!

---
+++ MergeSort.java
@@ -0,0 +1,107 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.io.*;
+import java.util.*;
+
+public class MergeSort {
+	
+	public class ComparableFile <T extends Comparable<T>> implements Comparable<ComparableFile<T>> {
+		
+		private final Deserializer<T> deserializer;
+	    private final Iterator<String> lines;
+	    private T buffered;
+	    
+	    public ComparableFile(File file, Deserializer<T> deserializer) {
+	        this.deserializer = deserializer;
+	        try {
+	            this.lines = Files.newBufferedReader(file.toPath()).lines().iterator();
+	        } catch (IOException e) {
+	            throw new UncheckedIOException(e);
+	        }
+	    }
+	    
+	    @Override
+	    public int compareTo(ComparableFile<T> that) {
+	        T first = peek();
+	        T second = that.peek();
+
+	        if (first == null) return second == null ? 0 : -1;
+	        if (second == null) return 1;
+	        return first.compareTo(second);
+	    }
+	    
+	    public T pop() {
+	        T tmp = peek();
+
+	        if (tmp != null) {
+	            buffered = null;
+	            return tmp;
+	        }
+
+	        throw new NoSuchElementException();
+	    }
+	    
+	    private T peek() {
+	        if (buffered != null) return buffered;
+	        if (!lines.hasNext()) return null;
+	        return buffered = deserializer.deserialize(lines.next());
+	    }
+	    
+	    public boolean isEmpty() {
+	        return peek() == null;
+	    }
+	}
+
+	public class MergeFiles<T extends Comparable<T>> {
+	    private final PriorityQueue<ComparableFile<T>> files;
+
+	    public MergeFiles(List<File> files, Deserializer<T> deserializer) {
+	        this.files = new PriorityQueue<>(files.stream()
+	                .map(file -> new ComparableFile<>(file, deserializer))
+	                .filter(comparableFile -> !comparableFile.isEmpty())
+	                .collect(toList()));
+	    }
+
+	    public Iterator<T> getSortedElements() {
+	        return new Iterator<T>() {
+	            @Override
+	            public boolean hasNext() {
+	                return !files.isEmpty();
+	            }
+
+	            @Override
+	            public T next() {
+	                if (!hasNext()) throw new NoSuchElementException();
+	                ComparableFile<T> head = files.poll();
+	                T next = head.pop();
+	                if (!head.isEmpty()) files.add(head);
+	                return next;
+	            }
+	        };
+	    }
+	}
+
+	private static File newTempFile(List<String> words) throws IOException {
+	    File tempFile = File.createTempFile("sorted-", ".txt");
+	    Files.write(tempFile.toPath(), words);
+	    tempFile.deleteOnExit();
+	    return tempFile;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		List<File> files = Arrays.asList(
+	            newTempFile(Arrays.asList("hello", "world")),
+	            newTempFile(Arrays.asList("english", "java", "programming")),
+	            newTempFile(Arrays.asList("american", "scala", "stackoverflow"))
+	    );
+
+	    Iterator<String> sortedElements = new MergeFiles<>(files, line -> line).getSortedElements();
+	    while (sortedElements.hasNext()) {
+	        System.out.println(sortedElements.next());
+	    }
+
+	}
+
+}
